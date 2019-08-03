bfedd65cc18750552f0aed9b6067969a20bf283d
Chris Allison <chris.charles.allison@gmail.com>
2019-08-03 16:48:13 +0100

    tvhdelete

---
+++ tvheadend/tvhdelete.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018, Christopher Allison
+#
+#     This file is part of tvh.
+#
+#     tvh is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvh is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvh.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Standalone tvheadend file delete for tvh application
+"""
+
+import sys
+import os
+import shutil
+import time
+import tvheadend.tvh as TVH
+import tvheadend.utils as UT
+import tvheadend.config as CONF
+import tvheadend.categories as CATS
+import tvheadend.nfo as NFO
+from tvheadend.errors import errorExit
+
+
+class TvhInputError(Exception):
+    pass
+
+
+def tvhdelete(fn):
+    try:
+        config = CONF.readConfig()
+        ipaddr = str(config["tvhipaddr"]) + ":" + str(config["tvhport"])
+        tvhauth = {"ip": ipaddr, "xuser": config["user"], "xpass": config["pass"]}
+        tot, ents = TVH.finishedRecordings(**tvhauth)
+        for show in ents:
+            if show["filename"] == fn:
+                print("Deleting {}".format(fn))
+                TVH.deleteRecording(show["uuid"], **tvhauth)
+    except Exception as e:
+        fname = sys._getframe().f_code.co_name
+        errorExit(fname, e)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        fn = sys.argv[1]
+    else:
+        raise(TvhInputError("Please supply a filename"))
+    tvhdelete(fn)
