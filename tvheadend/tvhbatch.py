0b03f54ae81c8af0e8a681ee7ee07737b28522a6
Chris Allison <chris.charles.allison@gmail.com>
2019-10-13 09:50:40 +0100

    don't convert HD broadcasts - they are alread h264 encoded

--- tvheadend/tvhbatch.py
+++ tvheadend/tvhbatch.py
@@ -109,8 +109,11 @@ def moveShow(show, config):
                 # as it tests whether this is a movie or not
                 removeFromYear(show, config)
                 # logout("\n")
-                logout("converting to mkv")
-                convertToMkv(opfn)
+                if show["channelname"].endswith("HD"):
+                    logout("Not converting HD programme {}".format(show["title"]))
+                else:
+                    logout("converting {} to mkv".format(show["title"]))
+                    convertToMkv(opfn)
         else:
             raise(CopyFailure("Failed to copy {} to {}".format(show["filename"], opfn)))
     except Exception as e:
