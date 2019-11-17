d8dc9a283a1679813f72903289a1a4b85f5f4eff
Chris Allison <chris.charles.allison@gmail.com>
2019-11-17 16:40:58 +0000

    we only need to store the fully qualifed file name

--- tvheadend/tvhdb.sql
+++ tvheadend/tvhdb.sql
@@ -1,21 +1,8 @@
 create table files (
     id integer auto increment,
-    name text,
-    size integer null,
-    hash text,
+    name text not null,
+    size integer null default 0,
+    hash text null default "",
     status integer null default 0
 );
 create index filename_idx on files(name);
-create index filestatus_idx on files(name, status);
-
-create table paths (
-    id integer auto increment,
-    name text
-);
-create index pathname_idx on paths(name);
-
-create table filepathmap (
-    id integer auto increment,
-    fileid integer not null,
-    pathid integer not null
-);
