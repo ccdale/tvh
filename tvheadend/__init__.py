0cc5117d194ee1ce6e6c02ad6b35e8843bc87e65
Chris Allison <chris.charles.allison@gmail.com>
2019-11-17 16:58:29 +0000

    v1.3.0 - logging, database of files to convert

--- tvheadend/__init__.py
+++ tvheadend/__init__.py
@@ -19,8 +19,8 @@
 init module for tvh application
 """
 majorv = 1
-minorv = 2
-buildv = 25
+minorv = 3
+buildv = 0
 versionl = (majorv, minorv, buildv)
 __version__ = ".".join(str(x) for x in versionl)
 
