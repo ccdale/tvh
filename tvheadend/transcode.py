b6c18fbdd66d5ef481e87de732735b7d1e797392
Chris Allison <chris.charles.allison@gmail.com>
2020-01-04 05:00:24 +0000

    init transcode window

--- tvheadend/transcode.py
+++ tvheadend/transcode.py
@@ -1,7 +1,7 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Pango
+from gi.repository import Gtk
 
 import tvheadend
 from tvheadend import __version__ as verstr
@@ -11,5 +11,16 @@ import tvheadend.tvhlog
 log = tvheadend.tvhlog.log
 
 
-class TranscodeWindow:
-    pass
+class TranscodeWindow(Gtk.Grid):
+    """ displays the list of programs to move to kodi and to be transcoded
+    """
+
+    def __init__(self, window, drama=[], documentary=[], comedy=[], music=[], films=[]):
+        log.debug("Transcode Window init")
+        super().__init__()
+        self.win = window
+        self.drama = drama
+        self.documentary = documentary
+        self.comedy = comedy
+        self.music = music
+        self.films = films
